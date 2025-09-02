c056f404586f80e8d26a6616b0df736792520129
abeaduhh <adiba.labonno@gmail.com>
2025-09-02 15:09:36 -0500

    this is a starting point

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      This is a Start
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/home_page/homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './homepage.css'
+
+function homepage() {
+
+  return (
+    <div>
+      This is a the home page
+    </div>
+  )
+}
+
+export default homepage
