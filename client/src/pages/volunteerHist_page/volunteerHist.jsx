620b6e82048aff3f9a7e80d0fc15a5acc129c61a
Alex Melendez <alexamelen.03@gmail.com>
2025-09-23 14:24:02 -0500

    starting point volunteer History

---
+++ client/src/pages/volunteerHist_page/volunteerHist.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './volunteerHist.css'
+import { useState } from "react";
+
+function volunteerHist() {
+
+    return (
+      <div>
+        <h1> Volunteer History</h1>
+      </div>
+    )
+  }
+  
+  export default volunteerHist;
