4faa94bd6536df7030dc020f34cd41b9ca54ac27
Alex Melendez <alexamelen.03@gmail.com>
2025-09-23 14:53:40 -0500

    establishing environment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/login_page/login.jsx'
 import Register from './pages/register_page/register.jsx'
 import EventManagement from './pages/admin_side/event_management.jsx'
 import VolunteerMatch from './pages/admin_side/volunteer_match.jsx'
+import VolunteerHist from './pages/volunteerHist_page/volunteerHist.jsx'
 import './App.css'
 
 function App() {
@@ -17,10 +18,13 @@ function App() {
       <Route path="/account" element={<Account />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register/>} />
+      <Route path="/history" element={<VolunteerHist/>} />
 
       {/* ADMINS */}
       <Route path="/eventmanagement" element={<EventManagement />} />
       <Route path="/volunteermatch" element={<VolunteerMatch />} />
+
+
 
     </Routes>
   )
